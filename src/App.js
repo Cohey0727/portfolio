6cae6719a22498926ed603d8475655ba148e5819
Cohey0727 <ohayousagi.ac.kook0727@gmail.com>
2019-03-24 15:52:24 +0900

    feature/create_redux_saga create redux saga

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import logo from './logo.svg';
+import store from './redux';
+import axios from 'axios';
+
 import './App.css';
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] =
+  'application/x-www-form-urlencoded';
+
+// axios.defaults.headers.common['Content-Type'] =
+//   'application/x-www-form-urlencoded';
+
+const myApi = axios.create({
+  baseURL: 'https://www.gaitameonline.com/rateaj/getrate',
+  timeout: 10000,
+  headers: {
+    Accept: 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept'
+  }
+});
+console.debug(myApi);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <button
+              onClick={() =>
+                store.dispatch({
+                  type: 'APP/EXCHANGE/READ'
+                })
+              }
+            />
+            <button
+              onClick={() => {
+                myApi
+                  .get()
+                  .then((res) => {
+                    console.debug(res);
+                  })
+                  .catch((res) => {
+                    console.debug(res);
+                  });
+              }}
+            />
+            <a
+              className='App-link'
+              href='https://reactjs.org'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Provider>
     );
   }
 }
