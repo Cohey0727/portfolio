a7aa77ada665c346855c3c9bf222c3f7098c0b48
Cohey0727 <ohayousagi.ac.kook0727@gmail.com>
2019-03-28 11:50:04 +0900

    feature/create_redux_saga test

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
           <header className='App-header'>
             <img src={logo} className='App-logo' alt='logo' />
             <p>
-              Edit <code>src/App.js</code> and save to reload.
+              Hello Edit <code>src/App.js</code> and save to reload.
             </p>
             <button
               onClick={() =>
