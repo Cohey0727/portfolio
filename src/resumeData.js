6428aa1d92285670e81ad9453f87d6dbade2c05f
Cohey0727 <ohayousagi.ac.kook0727@gmail.com>
2019-09-25 15:21:34 +0900

    master

--- src/resumeData.js
+++ src/resumeData.js
@@ -93,26 +93,11 @@ let resumeData = {
     }
   ],
   portfolio: [
-    {
-      name: "project1",
-      description: "mobileapp",
-      imgurl: "images/portfolio/phone.jpg"
-    },
-    {
-      name: "project2",
-      description: "mobileapp",
-      imgurl: "images/portfolio/project.jpg"
-    },
-    {
-      name: "project3",
-      description: "mobileapp",
-      imgurl: "images/portfolio/project2.png"
-    },
-    {
-      name: "project4",
-      description: "mobileapp",
-      imgurl: "images/portfolio/phone.jpg"
-    }
+    // {
+    //   name: "project1",
+    //   description: "hokan",
+    //   imgurl: "images/portfolio/phone.jpg"
+    // }
   ],
   testimonials: [
     {
